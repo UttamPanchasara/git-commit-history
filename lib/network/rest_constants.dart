69af19effa2b85a724a5df30a57347c74a6b5a9e
UttamPanchasara <panchasarauttam@gmail.com>
2020-01-07 15:10:36 +0530

    - Changed repo-url

--- lib/network/rest_constants.dart
+++ lib/network/rest_constants.dart
@@ -1,5 +1,5 @@
 class RestConstants {
   static const String BASE_URL = "https://api.github.com/";
   static const String GET_GIT_COMMIT_HISTORY =
-      "repos/afifPathan/Git-List/commits";
+      "repos/UttamPanchasara/git-commit-history/commits";
 }
